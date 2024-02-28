aaf07fd6278ebf9ce4996b0b234068a672fe5f43
Cloud <maniuuur@gmail.com>
2024-02-28 18:04:24 +0100

    Added messages resourse with examples from db, switched to partial and refactored some of the code

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'semantic-ui-sass'
 gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
+gem 'bcrypt', '~> 3.1', '>= 3.1.12'
+gem 'hirb'
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(username: "Andrew", password: "password")
+User.create(username: "Liz", password: "password")
+User.create(username: "Goku", password: "password")
+User.create(username: "Doug", password: "password")
+User.create(username: "Annie", password: "password")
